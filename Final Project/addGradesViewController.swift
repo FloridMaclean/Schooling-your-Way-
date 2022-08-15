a089e9140da4fdf3ff5e76dbfd30f35188a3b814
Sanket Patel <patelsanketr98@gmail.com>
2022-08-14 23:12:06 -0400

    add grades added with segue from professor controller

---
+++ Final Project/addGradesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  addGradesViewController.swift
+//  Final Project
+//
+//  Created by Sanket Patel on 2022-08-14.
+//
+
+import UIKit
+
+class addGradesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+ 
+
+    @IBAction func onBackButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
