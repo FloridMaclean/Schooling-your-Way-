61ae212521d95e9136572fe6e9abd48c38d35693
Florid Maclean <floridmaclean@gmail.com>
2022-08-15 04:42:30 -0400

    Fetching student details from the Firestore Database and displaying it to the student profile-commit.

--- Final Project/parentViewController.swift
+++ Final Project/parentViewController.swift
@@ -7,26 +7,52 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class parentViewController: UIViewController {
     
     @IBOutlet weak var studentNameLabel: UILabel!
+    @IBOutlet weak var studentIdLabel: UILabel!
+    @IBOutlet weak var studentEmailLabel: UILabel!
+    @IBOutlet weak var studentAddressLael: UILabel!
+    
+    let db = Firestore.firestore()
     
     private var goToGrades = "goToGradesViewController"
     private var goToEvents = "goToEventsViewController"
     
-    @IBOutlet weak var studentEmailLabel: UILabel!
-    var studentId: String?
+    var studentEmail: String?
     var userName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchStudentProfile()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == goToGrades {
             let destination = segue.destination as! gradesViewController
-            destination.studentId = studentId
+            destination.studentId = studentEmail
+        }
+    }
+    
+    private func fetchStudentProfile() {
+        let docRef = db.collection("student").document(studentEmail ?? "stud1")
+
+        docRef.getDocument { [self] (document, error) in
+            if let document = document, document.exists {
+                let address = document.data()!["Address"]! as AnyObject as! String
+                let name = document.data()!["name"]! as AnyObject as! String
+                let studentId = document.data()!["studentId"]! as AnyObject as! String
+                
+                studentNameLabel.text = name
+                studentEmailLabel.text = studentEmail
+                studentIdLabel.text = studentId
+                studentAddressLael.text = address
+                
+            } else {
+                print("Document does not exist")
+            }
         }
     }
  
